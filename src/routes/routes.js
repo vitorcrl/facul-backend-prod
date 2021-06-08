2e3d77141d6488b8fe753f4659673b4cd1bcecbd
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-06-08 03:14:02 -0300

    edit detail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 router.post("/produto", ProdutoController.Insert);
 router.get("/produto", ProdutoController.SelectAll);
 router.get("/produto/:id", ProdutoController.SelectDetail);
-router.put("/produto/:id", ProdutoController.Update);
-router.delete("/produto/:id", ProdutoController.Delete);
+router.put("/produtoedit/:id", ProdutoController.Update);
+router.delete("/produtodelete/:id", ProdutoController.Delete);
 
 module.exports = router;
 // subir servidor node index.js
